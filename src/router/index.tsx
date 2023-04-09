7edaa8cdbc2bab02741c1ea56fa0fbb60c9ae9c2
yxp2918 <2918369878@qq.com>
2023-04-09 19:47:30 +0800

    add Login SignUp

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,12 +3,15 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Index from '../container/Index'
 import About from '../container/About'
-
+import SignUp from '../container/SignUp'
+import Login from '../container/Login'
 const AllRoutes: React.FC = () => {
   return (
     <Routes>
-      <Route index path="/" element={<Index /> } />
+      <Route index path="/" element={<Index />} />
       <Route path="/about" element={<About />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   )
 }
